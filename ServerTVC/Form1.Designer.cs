87d2c2cecc09f6fb5f9ee16cf8dde643b79069d1
IrinaCinnabar <irinaesaulenko4190@gmail.com>
2021-07-02 01:24:07 +0300

    first commit

---
+++ ServerTVC/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ServerTVC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCondition = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelConnection = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Состояние сервера:";
+            // 
+            // labelCondition
+            // 
+            this.labelCondition.AutoSize = true;
+            this.labelCondition.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCondition.Location = new System.Drawing.Point(222, 19);
+            this.labelCondition.Name = "labelCondition";
+            this.labelCondition.Size = new System.Drawing.Size(112, 25);
+            this.labelCondition.TabIndex = 1;
+            this.labelCondition.Text = "Отключен";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Подключение к БД:";
+            // 
+            // labelConnection
+            // 
+            this.labelConnection.AutoSize = true;
+            this.labelConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelConnection.Location = new System.Drawing.Point(223, 70);
+            this.labelConnection.Name = "labelConnection";
+            this.labelConnection.Size = new System.Drawing.Size(125, 24);
+            this.labelConnection.TabIndex = 3;
+            this.labelConnection.Text = "Отсутствует";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(223, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Время до проверки БД:";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(251, 129);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(16, 24);
+            this.labelTime.TabIndex = 5;
+            this.labelTime.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 182);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelConnection);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelCondition);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCondition;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelConnection;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
+
---
+++ ServerTVC/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ServerTVC
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string[]> data1 = new List<string[]>();
+        List<string[]> data2 = new List<string[]>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Server(80);
+        }
+
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            string sqlExpression = "SELECT * FROM PMServiceLogs WHERE ErrStatus = 1 AND ID >" + Properties.Settings.Default.PMServiseLogs_id + "  ORDER BY ID";
+            string sqlExpression1 = "SELECT * FROM PMServiceLogs_H WHERE ErrStatus = 1 AND ID >" + Properties.Settings.Default.PMServiseLogs_H_id + " ORDER BY ID";
+            string sqlExpression2 = "SELECT * FROM PointMasterLogs WHERE (Msg LIKE '%Error%' OR Msg LIKE '%error%' OR Msg LIKE '%ERROR%') AND ID >" + Properties.Settings.Default.PMServiseLogs_H_id + " ORDER BY ID";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.bdlink))
+                {
+                    connection.Open();
+                    Console.WriteLine("Подключение открыто");
+                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+
+                    List<string> list = new List<string>();
+                    if (reader.HasRows) // если есть данные
+                    {
+                        // выводим названия столбцов
+                        //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}", reader.GetName(0), reader.GetName(1), reader.GetName(2), reader.GetName(3), reader.GetName(4), reader.GetName(5), reader.GetName(6), reader.GetName(7), reader.GetName(8));
+
+                        while (reader.Read()) // построчно считываем данные
+                        {
+                            data1.Add(new string[10]);
+                            data1[data1.Count - 1][0] = reader[0].ToString();
+                            data1[data1.Count - 1][1] = " ";
+                            data1[data1.Count - 1][2] = reader[1].ToString();
+                            data1[data1.Count - 1][3] = reader[2].ToString();
+                            data1[data1.Count - 1][4] = reader[3].ToString();
+                            data1[data1.Count - 1][5] = reader[4].ToString();
+                            data1[data1.Count - 1][6] = reader[5].ToString();
+                            data1[data1.Count - 1][7] = reader[6].ToString();
+                            data1[data1.Count - 1][8] = reader[7].ToString();
+                            data1[data1.Count - 1][9] = reader[8].ToString();
+                            if (!list.Contains(reader[4].ToString()))
+                            {
+                                list.Add(reader[4].ToString());
+                            }
+                        }
+                    }
+                    reader.Close();
+
+                    SqlCommand command1 = new SqlCommand(sqlExpression1, connection);
+                    SqlDataReader reader1 = command1.ExecuteReader();
+
+                    if (reader1.HasRows) // если есть данные
+                    {
+
+
+                        while (reader1.Read()) // построчно считываем данные
+                        {
+                            data1.Add(new string[10]);
+                            data1[data1.Count - 1][0] = reader1[0].ToString();
+                            data1[data1.Count - 1][1] = " ";
+                            data1[data1.Count - 1][2] = reader1[1].ToString();
+                            data1[data1.Count - 1][3] = reader1[2].ToString();
+                            data1[data1.Count - 1][4] = reader1[3].ToString();
+                            data1[data1.Count - 1][5] = reader1[4].ToString();
+                            data1[data1.Count - 1][6] = reader1[5].ToString();
+                            data1[data1.Count - 1][7] = reader1[6].ToString();
+                            data1[data1.Count - 1][8] = reader1[7].ToString();
+                            data1[data1.Count - 1][9] = reader1[8].ToString();
+                            if (!list.Contains(reader1[4].ToString()))
+                            {
+                                list.Add(reader1[4].ToString());
+                            }
+                        }
+                    }
+
+                    reader1.Close();
+
+                    SqlCommand command2 = new SqlCommand(sqlExpression2, connection);
+                    SqlDataReader reader2 = command2.ExecuteReader();
+
+                    if (reader2.HasRows) // если есть данные
+                    {
+
+
+                        while (reader2.Read()) // построчно считываем данные
+                        {
+                            data2.Add(new string[10]);
+                            data2[data2.Count - 1][0] = reader2[0].ToString();
+                            data2[data2.Count - 1][1] = reader2[1].ToString();
+                            data2[data2.Count - 1][2] = reader2[5].ToString();
+                            data2[data2.Count - 1][3] = reader2[2].ToString();
+                            data2[data2.Count - 1][4] = " ";
+                            data2[data2.Count - 1][5] = " ";
+                            data2[data2.Count - 1][6] = reader2[3].ToString();
+                            data2[data2.Count - 1][7] = reader2[4].ToString();
+                            data2[data2.Count - 1][8] = " ";
+                            data2[data2.Count - 1][9] = reader2[6].ToString();
+                            //if (!list.Contains(reader2[4].ToString()))
+                            //{
+                            //    list.Add(reader2[4].ToString());
+                            //}
+                        }
+                    }
+
+                    reader2.Close();
+
+                    /*
+                    foreach (string[] s in data1) {
+
+                    }
+                        dataGridView1.Rows.Add(s);
+
+
+                    foreach (string[] s in data2)
+                        dataGridView1.Rows.Add(s);
+                        */
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте настройки подключения к базе данных, либо наличие необходимых таблиц", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+    }
+    class Server
+    {
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+
+        // Запуск сервера
+        public Server(int Port)
+        {
+            // Создаем "слушателя" для указанного порта
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start(); // Запускаем его
+
+            // В бесконечном цикле
+            while (true)
+            {
+                // Принимаем новых клиентов
+                new Client(Listener.AcceptTcpClient());
+            }
+        }
+
+        // Остановка сервера
+        ~Server()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+
+    }
+
+    class Client
+    {
+        // Конструктор класса. Ему нужно передавать принятого клиента от TcpListener
+        public Client(TcpClient Client)
+        {
+            // Код простой HTML-странички
+            string Html = "<html><body><h1>It works!</h1></body></html>";
+            // Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
+            string Str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
+            // Приведем строку к виду массива байт
+            byte[] Buffer = Encoding.ASCII.GetBytes(Str);
+            // Отправим его клиенту
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            // Закроем соединение
+            Client.Close();
+        }
+    }
+
+}
